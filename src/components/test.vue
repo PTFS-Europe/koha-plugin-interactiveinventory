25036b9aa8b8287fe44d1ebf62749ef5274833bc
Jacob O'Mara <jacob.omara@ptfs-europe.com>
2024-07-25 16:09:59 +0100

    swap to api endpoints already defined for item
    also query the biblios endpoints to get all the information required to
    display.

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,9 +1,3 @@
-<style scoped>
-.test {
-  color: red;
-}
-</style>
-
 <template>
   <div>
     <InventorySetupForm @start-session="initiateInventorySession" v-if="!sessionStarted" />
@@ -14,8 +8,8 @@
         <button type="submit">Submit</button>
       </form>
       <div id="inventory_results">
-        <div v-for="item in items" :key="item['barcode']">
-          <p>{{ item['barcode'] }} - {{ item['itemlost'] }}</p>
+        <div v-for="item in items" :key="item.external_id">
+          <p>{{ item.biblio.title }} - {{ item.external_id }}</p>
         </div>
       </div>
     </div>
@@ -33,38 +27,65 @@ export default {
     return {
       barcode: '',
       items: [],
-      sessionStarted: false,
-      sessionData: {},
-      csrfToken: window.csrf_token,
+      sessionData: null,
+      sessionStarted: false
     }
   },
   methods: {
+    async submitBarcode() {
+      try {
+        // Fetch item data
+        const itemResponse = await fetch(
+          `/api/v1/items?external_id=${encodeURIComponent(this.barcode)}`,
+          {
+            method: 'GET',
+            // headers: {
+            //   'Content-Type': 'application/json'
+            // }
+          }
+        )
+        if (!itemResponse.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const itemText = await itemResponse.text();
+        const itemsArray = itemText ? JSON.parse(itemText) : [];
+        const itemData = itemsArray[0] || {};
+        console.log('Item data:', itemData)
+
+        // Fetch biblio data using biblio_id from item data
+        const biblioResponse = await fetch(
+          `/api/v1/biblios/${itemData.biblio_id}`,
+          {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json'
+            }
+          }
+        )
+        if (!biblioResponse.ok) {
+          throw new Error('Network response was not ok')
+        }
+       const biblioText = await biblioResponse.text();
+        const biblioData = biblioText ? JSON.parse(biblioText) : {};
+        window.biblioData = biblioData;
+        console.log('Biblio data:', biblioData)
+
+        // Combine item data and biblio data
+        const combinedData = { ...itemData, biblio: biblioData };
+        window.combinedData = combinedData;
+
+        // Prepend the combined data to the items array
+        this.items.unshift(combinedData);
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error)
+      }
+    },
     async initiateInventorySession(sessionData) {
       this.sessionData = sessionData
       this.sessionStarted = true
       try {
-    const response = await fetch(
-      `/cgi-bin/koha/plugins/run.pl?class=Koha::Plugin::Com::InteractiveInventory&method=start_session&session_data=${encodeURIComponent(JSON.stringify(sessionData))}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    )
-    if (!response.ok) {
-      throw new Error('Network response was not ok')
-    }
-    const data = await response.json()
-    console.log('Session started:', data)
-  } catch (error) {
-    console.error('There was a problem with the fetch operation:', error)
-  }
-},
-    async submitBarcode() {
-      try {
         const response = await fetch(
-          `/cgi-bin/koha/plugins/run.pl?class=Koha::Plugin::Com::InteractiveInventory&method=get_item_data&barcode=${encodeURIComponent(this.barcode)}`,
+          `/cgi-bin/koha/plugins/run.pl?class=Koha::Plugin::Com::InteractiveInventory&method=start_session&session_data=${encodeURIComponent(JSON.stringify(sessionData))}`,
           {
             method: 'GET',
             headers: {
@@ -75,10 +96,10 @@ export default {
         if (!response.ok) {
           throw new Error('Network response was not ok')
         }
-        const data = await response.json()
-        this.items.unshift(data)
+        const data = await response.json();
+        console.log('Session started:', data);
       } catch (error) {
-        console.error('There was a problem with the fetch operation:', error)
+        console.error('There was a problem with the fetch operation:', error);
       }
     }
   }
@@ -88,12 +109,5 @@ export default {
 <style scoped>
 form {
   display: flex;
-  flex-direction: column;
-}
-label {
-  margin-top: 10px;
-}
-button {
-  margin-top: 20px;
 }
 </style>
